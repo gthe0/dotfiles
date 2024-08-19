8e062c4ef9a543b803a2af3e3fa04ba72858e0e1
gtheo <89408641+gthe0@users.noreply.github.com>
2024-08-19 10:30:59 +0300

    Did not delete the files of the previous configuration

--- nvim/lua/config/mappings.lua
+++
@@ -1,7 +0,0 @@
-local keymap = vim.keymap
-
-vim.g.mapleader = " "		-- Remapping Leader Key to space
-
-keymap.set('n', '<leader>cd', '<cmd>lcd%:h<CR>', vim.opts) -- Change current working directory to the current file
-keymap.set('n', '<leader>b', '<cmd>:buffers<CR>:buffer<Space>', vim.opts) -- Display buffer list and then select the one that you want to
-
