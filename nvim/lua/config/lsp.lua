57b81684b368b16e40d7b19f792cd91a3e20c9e1
gtheo <giorgostheodoraki@hotmail.com>
2026-03-13 12:18:28 +0200

    AAA

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,4 +1,7 @@
-vim.opt.completeopt = { "menuone", "noselect", "popup" }
+vim.o.complete = ".,o"
+vim.o.completeopt = { "fuzzy", "menuone", "noselect", "popup" }
+vim.o.autocomplete = true
+vim.o.pumheight = 8
 
 vim.lsp.enable({
     "clangd",
@@ -9,58 +12,17 @@ vim.lsp.enable({
     "bash-language-server"
 })
 
--- Trigger autocompletion
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('my.lsp', {}),
-    callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-        if client:supports_method('textDocument/implementation') then
-            -- Keymaps (only the ones you asked to keep)
-            local opts = { buffer = bufnr, silent = true }
-            local keymap = vim.keymap
-
-            keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-            keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-            keymap.set("n", "gr", vim.lsp.buf.references, opts)
-            keymap.set("n", "<leader>t", vim.lsp.buf.type_definition, opts)
-            keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
-            --  vertical split‑definition
-            keymap.set("n", "<leader>d", function()
-                if vim.fn.winnr("$") > 1 then
-                    vim.cmd([[vsplit]])
-                    vim.lsp.buf.definition()
-                    local current_buf = vim.api.nvim_get_current_buf()
-
-                    vim.cmd("wincmd c")
-                    vim.cmd("wincmd w")
-
-                    vim.api.nvim_set_current_buf(current_buf)
-                else
-                    vim.cmd([[vsplit]])
-                    vim.lsp.buf.definition()
-                end
-            end, opts)
-        end
-
-        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, args.buf, {
-                autotrigger = true,
-                convert = function(item)
-                    return { abbr = item.label:gsub("%b()", "") }
-                end,
-            })
-        end
-    end,
+-- nvim diagnostics enable
+vim.diagnostic.config({
+    update_in_insert = true,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+    }
 })
 
 -- Go to prev/next diagnostics and open a float
 vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, { desc = "previous diagnostics" })
 vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count =  1, float = true }) end, { desc = "next diagnostics" })
-
--- Enable and attach
 
